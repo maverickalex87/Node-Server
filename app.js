1f729523c32bfcad3caa5781165ef9777b8f6d61
alexlupu <alexcristi26@yahoo.com>
2024-03-06 23:47:09 +0100

    route 404

--- app.js
+++ app.js
@@ -10,8 +10,11 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: false}));
 
 
-app.use(adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
+app.use((req, res, next) => {
+  res.status(404).send('<h1> Page Not Found ! </h1>');
+});
 
 app.listen(3000);

8138fcc522b70b3b5bd835afcc43d45809301356
alexlupu <alexcristi26@yahoo.com>
2024-03-08 12:26:43 +0100

    handlebars

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const expressHbs = require('express-handlebars');
 
 
 const app = express();
 
-app.set('view engine', 'pug');
+app.engine('hbs', expressHbs.engine({layoutsDir: '/views/layouts'}));
+app.set('view engine', 'hbs');
 app.set('views', 'views');
 
 const adminData = require('./routes/admin');
@@ -22,7 +24,7 @@ app.use(shopRoutes);
 
 
 app.use((req, res, next) => {
-  res.status(404).render('404');
+  res.status(404).render('404', {pageTitle: 'Page Not Found'});
 });
 
 app.listen(3000);

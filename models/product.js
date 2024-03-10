863b868ba7c2878de48ee05e18826b3484967cbe
alexlupu <alexcristi26@yahoo.com>
2024-03-10 21:12:15 +0100

    models copmlete part 1

--- models/product.js
+++ models/product.js
@@ -1,5 +1,16 @@
 const fs = require('fs');
 const path = require('path');
+const p = path.join(path.dirname(require.main.filename), 'data', 'products.json');
+
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
 
 module.exports = class Product {
   constructor(title, price, description, imageUrl) {
@@ -10,12 +21,7 @@ module.exports = class Product {
   }
 
   save() {
-    const p = path.join(path.dirname(require.main.filename), 'data', 'products.json');
-    fs.readFile(p, (err, fileContent) => {
-      let products = [];
-      if (!err) {
-        products = JSON.parse(fileContent);
-      }
+    getProductsFromFile(products => {
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), (err) => {
         console.log(err);
@@ -24,12 +30,6 @@ module.exports = class Product {
   }
 
   static fetchAll(cb) {
-    const p = path.join(path.dirname(require.main.filename), 'data', 'products.json');
-    fs.readFile(p, (err, fileContent) => {
-      if (err) {
-        cb([]);
-      }
-      cb(JSON.parse(fileContent));
-    })
+    getProductsFromFile(cb);
   }
-}
+};

1893fb4b3a67ae7b1feb40b950b5b516571e3171
alexlupu <alexcristi26@yahoo.com>
2024-04-25 18:32:41 +0200

    fv

--- controllers/admin.js
+++ controllers/admin.js
@@ -42,7 +42,6 @@ exports.getEditProduct = (req, res, next) => {
   }
   const prodId = req.params.productId;
   Product.findById(prodId)
-    // Product.findById(prodId)
     .then(product => {
       if (!product) {
         return res.redirect('/');
@@ -65,14 +64,9 @@ exports.postEditProduct = (req, res, next) => {
   const updatedImageUrl = req.body.imageUrl;
 
 
-      const product = new Product(
-        updatedTitle,
-        updatedPrice,
-        updatedDesc,
-        updatedImageUrl,
-        new ObjectId(prodId)
-        );
-    product.save()
+    Product.findById(prodId).then(product => {
+      product.save()
+    })
     .then(result => {
       console.log('UPDATED PRODUCT!');
       res.redirect('/admin/products');
@@ -81,7 +75,7 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
+  Product.find()
     .then(products => {
       res.render('admin/products', {
         prods: products,
--- routes/admin.js
+++ routes/admin.js
@@ -13,9 +13,9 @@ router.get('/add-product', adminController.getAddProduct);
 router.get('/products', adminController.getProducts);
 
 // // /admin/add-product => POST
-// router.post('/add-product', adminController.postAddProduct);
+router.post('/add-product', adminController.postAddProduct);
 
-// router.get('/edit-product/:productId', adminController.getEditProduct);
+router.get('/edit-product/:productId', adminController.getEditProduct);
 
 // router.post('/edit-product', adminController.postEditProduct);
 

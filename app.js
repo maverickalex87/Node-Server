4cff2d4410e05e3dac52be75c4cfa2fc0be5b58d
alexlupu <alexcristi26@yahoo.com>
2024-05-07 13:23:05 +0200

    relation userid/product

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
 
-// const User = require('./models/user');
+const User = require('./models/user');
 
 const app = express();
 
@@ -21,14 +21,14 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((req, res, next) => {
-//   User.findById('660001e0f66bb8d752e5d410')
-//   .then(user => {
-//     req.user = new User(user.name, user.email, user.cart, user._id);
-//     next();
-//   })
-//   .catch(err =>  console.log(err));
-// });
+app.use((req, res, next) => {
+  User.findById('663a0c117730982c1e698f37')
+  .then(user => {
+    req.user = user;
+    next();
+  })
+  .catch(err =>  console.log(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -40,6 +40,18 @@ mongoose.connect(
   'mongodb+srv://alex:Casandra87@cluster0.c38dv6i.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster0'
 )
 .then(result => {
+  User.findOne().then(user => {
+    if (!user) {
+      const user = new User ({
+        name: 'Alex',
+        email: 'alexcristi26@yahoo.com',
+        cart: {
+          items: []
+        }
+      });
+      user.save();
+    }
+  })
   app.listen(3000);
 })
 .catch(err => {

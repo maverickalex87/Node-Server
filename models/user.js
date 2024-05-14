f703be2e6ca5217c498076448287ed5acc7da3de
alexlupu <alexcristi26@yahoo.com>
2024-05-14 13:15:26 +0200

    add orders

--- models/user.js
+++ models/user.js
@@ -1,3 +1,4 @@
+const { ReturnDocument } = require('mongodb');
 const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
@@ -13,7 +14,7 @@ const userSchema = new Schema ({
   },
   cart: {
     items: [{
-      productId : {
+      productId: {
         type: Schema.Types.ObjectId,
         ref: 'Product',
         required: true
@@ -25,6 +26,43 @@ const userSchema = new Schema ({
     }]
   }
 });
+
+userSchema.methods.addToCart = function(product) {
+  const cartProductIndex = this.cart.items.findIndex(cp => {
+      return cp.productId.toString() === product._id.toString();
+    });
+    let newQuantity = 1;
+    const updatedCartItems = [ ...this.cart.items ];
+
+    if (cartProductIndex >= 0) {
+      newQuantity = this.cart.items[cartProductIndex].quantity + 1
+      updatedCartItems[cartProductIndex].quantity = newQuantity;
+    } else {
+      updatedCartItems.push({ productId: product._id, quantity: newQuantity });
+    }
+
+    const updatedCart = {
+      items: updatedCartItems
+    };
+    this.cart = updatedCart;
+    return this.save();
+};
+
+userSchema.methods.removeFromCart = function(productId) {
+  const updatedCartItems = this.cart.items.filter(item => {
+    return item.productId.toString() !== productId.toString();
+  });
+  this.cart.items = updatedCartItems;
+  return this.save();
+};
+
+// userSchema.methods.getOrders = function(productId) {
+//   const orders = getDb();
+//     return db
+//       .collection('orders')
+//       .find({'user._id': new ObjectId(this._id)})
+//       .toArray();
+// }
 
 module.exports = mongoose.model('User', userSchema);
 

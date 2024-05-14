79b3277d8b483cb54865c08d34fae1b0bccfbbc3
alexlupu <alexcristi26@yahoo.com>
2024-05-14 20:43:24 +0200

    login

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 
 
 app.use(bodyParser.urlencoded({extended: false}));
@@ -32,6 +33,7 @@ app.use((req, res, next) => {
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 
 app.use(errorController.get404);

905beff26923860194a98fee8538feff1d2567fe
alexlupu <alexcristi26@yahoo.com>
2024-05-15 21:30:37 +0200

    mongodb-session

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,15 @@
 exports.getLogin = (req, res, next) => {
-  const isLoggedIn = req.get('Cookie').split("=")[1].split('D')[1] === 'true';
+  // const isLoggedIn = req.get('Cookie').split("=")[1].split('D')[1] === 'true';
+  console.log(req.session.isLoggedIn);
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
-    isAuthenticated: isLoggedIn
+    isAuthenticated: true
+
   });
 };
 
 exports.postLogin = (req, res, next) => {
-  res.setHeader('isAuthenticated', 'loggedIn=true');
+  req.session.isLoggedIn = true; // Access the session object using 'req.session'
   res.redirect('/');
 };

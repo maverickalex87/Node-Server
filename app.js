d0994b4cec1a063635714c3f457c0722b8ec4086
alexlupu <alexcristi26@yahoo.com>
2024-05-22 22:27:22 +0200

    validation password encripted

--- app.js
+++ app.js
@@ -61,22 +61,9 @@ app.use(authRoutes);
 
 app.use(errorController.get404);
 
-mongoose.connect(
-  MONGODB_URI
-)
+mongoose
+.connect(MONGODB_URI)
 .then(result => {
-  User.findOne().then(user => {
-    if (!user) {
-      const user = new User ({
-        name: 'Alex',
-        email: 'alexcristi26@yahoo.com',
-        cart: {
-          items: []
-        }
-      });
-      user.save();
-    }
-  })
   app.listen(3000);
 })
 .catch(err => {
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
-const User = require("../models/user");
+const User = require('../models/user');
+const bcrypt = require('bcryptjs');
 
 exports.getLogin = (req, res, next) => {
   res.render("auth/login", {
@@ -9,21 +10,21 @@ exports.getLogin = (req, res, next) => {
 };
 
 exports.getSignup = (req, res, next) => {
-  res.render("auth/signup", {
-    path: "/signup",
-    pageTitle: "Signup",
+  res.render('auth/signup', {
+    path: '/signup',
+    pageTitle: 'Signup',
     isAuthenticated: false,
   });
 };
 
 exports.postLogin = (req, res, next) => {
-  User.findById("664de9e9b7486dfd5c1db1bd")
+  User.findById('664de9e9b7486dfd5c1db1bd')
     .then((user) => {
       req.session.isLoggedIn = true;
       req.session.user = user;
       req.session.save((err) => {
         console.log(err);
-        res.redirect("/");
+        res.redirect('/');
       });
     })
     .catch((err) => console.log(err));
@@ -31,6 +32,30 @@ exports.postLogin = (req, res, next) => {
 
 exports.postSignup = (req, res, next) => {
   const email = req.body.email;
+  const password = req.body.password;
+  const confirmPassword = req.body.confirmPassword;
+  User.findOne({ email: email })
+  .then(userDoc => {
+    if (userDoc) {
+      return res.redirect('/signup');
+    }
+    return bcrypt.hash(password, 12);
+  })
+  .then(hashedPassword => {
+    const user = new User({
+      email: email,
+      password: hashedPassword,
+      cart: { items: [] }
+    });
+    return user.save();
+
+  })
+  .then(result => {
+    res.redirect('/login');
+  })
+  .catch(err => {
+    console.log(err);
+  });
 };
 
 exports.postLogout = (req, res, next) => {

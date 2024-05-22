d49a1b695268a1e5e0027394a56d2cf428d6f8ca
alexlupu <alexcristi26@yahoo.com>
2024-05-22 21:06:15 +0200

    vscode

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,29 +1,41 @@
-const User = require('../models/user');
+const User = require("../models/user");
 
 exports.getLogin = (req, res, next) => {
-  res.render('auth/login', {
-    path: '/login',
-    pageTitle: 'Login',
-    isAuthenticated: false
+  res.render("auth/login", {
+    path: "/login",
+    pageTitle: "Login",
+    isAuthenticated: false,
+  });
+};
+
+exports.getSignup = (req, res, next) => {
+  res.render("auth/signup", {
+    path: "/signup",
+    pageTitle: "Signup",
+    isAuthenticated: false,
   });
 };
 
 exports.postLogin = (req, res, next) => {
-  User.findById('664de9e9b7486dfd5c1db1bd')
-    .then(user => {
+  User.findById("664de9e9b7486dfd5c1db1bd")
+    .then((user) => {
       req.session.isLoggedIn = true;
       req.session.user = user;
-      req.session.save(err => {
+      req.session.save((err) => {
         console.log(err);
-        res.redirect('/');
+        res.redirect("/");
       });
     })
-    .catch(err => console.log(err));
+    .catch((err) => console.log(err));
+};
+
+exports.postSignup = (req, res, next) => {
+  const email = req.body.email;
 };
 
 exports.postLogout = (req, res, next) => {
-  req.session.destroy(err => {
+  req.session.destroy((err) => {
     console.log(err);
-    res.redirect('/');
+    res.redirect("/");
   });
 };
